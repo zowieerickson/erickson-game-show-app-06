eaab85c969e0f4dabe2cfc65e8d881434f451cf8
Zowie Erickson <zowie.erickson@gmail.com>
2021-03-24 16:56:04 -0400

    beginning checkLetter function to match letter key target

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const overlay = document.getElementById("overlay");
 const btnReset = document.querySelector("a.btn__reset");
 const phrase = document.getElementById("phrase");
 const qwerty = document.getElementById("qwerty");
+const ul = document.querySelector("#phrase ul");
 let missed = 0;
 
 const phrases = [
@@ -23,7 +24,6 @@ function getRandomPhraseAsArray(arr) {
 }
 
 function addPhraseToDisplay(arr) {
-  const ul = document.querySelector("#phrase ul");
   for (let i = 0; i < arr.length; i++) {
     const character = arr[i];
     for (let i = 0; i < character.length; i++) {
@@ -34,11 +34,22 @@ function addPhraseToDisplay(arr) {
         li.className = "letter";
       }
     }
-    // console.log(character);
   }
 }
 
-//
-// lets go
+function checkLetter(btn) {
+  const characters = document.getElementsByClassName("letter");
+  for (let i = 0; i < characters.length; i++) {
+    const character = characters[i];
+    if (character == btn.textContent) {
+      li.classList.add("show");
+      const matchingCharacter = character; // Not sure if this works
+      return matchingCharacter; // Same, not sure if will work if the one on top doesn't work
+    } else {
+      return null;
+    }
+  }
+}
+
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
